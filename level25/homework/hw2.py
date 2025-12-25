46f0c9b92a3f3f86ffb931260ca18d37f186966a
zazamchedldize <mchedlidze.zaza1000@gmail.com>
2025-12-25 21:04:01 +0400

    second commit

--- level25/homework/hw2.py
+++ level25/homework/hw2.py
@@ -1,9 +1,9 @@
 # 2) შექმენი list: fruits = ["apple", "banana", "apple", "orange"] მომხმარებელს შეაყვანინე ხილი, თუ list-ში უკვე არის ეს ხილი remove()-ით წაშალე მხოლოდ პირველი შემხვედრი, თუ არ არის ლისტში მაშინ დაბეჭდე შესაბამისი შეტყობინება
 
 fruits = ["apple", "banana", "apple", "orange"]
-name = input("შეიყვანე ხილი: ")
-if name in fruits:
-    fruits.remove(name)
+fruit = input("შეიყვანე ხილი: ")
+if fruit in fruits:
+    fruits.remove(fruit)
     print("ხილი წაშლილია.")
 else:
     print("ხილი არ არის სიაში.")
