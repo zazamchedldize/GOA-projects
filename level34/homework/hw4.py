7e4a16b88c587d172fd989f6434aae8b6bc236fa
zazamchedldize <mchedlidze.zaza1000@gmail.com>
2026-02-03 21:47:19 +0400

    second commit

--- level34/homework/hw4.py
+++ level34/homework/hw4.py
@@ -1,16 +1,14 @@
 # 4) შექმენით ფუნქცია. მომხმარებელს შემოატანინეთ წინადადების სტრინგი. დათვალე, რამდენი სიტყვის სიგრძე არის 4-ზე მეტი. დაპრინტე ასეთი სიტყვების რაოდენობა. დაწერეთ ეს დავალება ორნაირად - split() ფუნქციით და split() ფუნქციის გარეშე.
 
-def string():
-    sentence = input("შეიყვანეთ წინადადება: ")
-    words = sentence.split()
+def count_words():
+    text = input("შემოიყვანეთ წინადადება: ")
+    words = text.split()
     count = 0
 
-    for word in words:
-        if len(word) > 4:
+    for i in range(len(words)):
+        if len(words[i]) > 4:
             count += 1
 
     print(count)
 
-string()
-
-
+count_words()
