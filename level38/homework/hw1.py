fac356da12e48a6bfbe716fc69b6c42629db5618
zazamchedldize <mchedlidze.zaza1000@gmail.com>
2026-02-14 12:33:55 +0400

    second commit

---
+++ level38/homework/hw1.py
@@ -0,0 +1,29 @@
+#codewars 1
+# def repeat_str(repeat, string):
+#     return string * repeat
+
+#codewars 2
+# def get_count(sentence):
+#     count = 0
+#     vowels = "aeiou"
+#     for i in sentence:
+#         if i in vowels:
+#             count += 1
+#     return count
+
+#codewars 3
+# def positive_sum(arr):
+#     sum = 0
+#     for i in arr:
+#         if i > 0:
+#             sum += i
+#     return sum        
+
+#codewars 4
+# def string_to_number(s):
+#     return int(s)
+
+#codewars 5
+# def greet():
+#     return "hello world!"
+# print(greet()) 
