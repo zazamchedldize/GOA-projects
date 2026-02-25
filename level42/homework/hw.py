a3849d9c19c95793bc7cedb84c88ee82fe0c839e
zazamchedldize <mchedlidze.zaza1000@gmail.com>
2026-02-25 21:53:55 +0400

    second commitv

---
+++ level42/homework/hw.py
@@ -0,0 +1,37 @@
+# codewars 1
+# def remove_char(s):
+#     if len(s) == 2:
+#         return ""
+#     return s[1:-1]
+
+# codewars 2
+# def double_integer(i):
+#    return i*2
+
+# codewars 3
+# def greet(name):
+#     return "Hello, " + name + " how are you doing today?"
+# greet("zaza")    
+
+# codewars 4    
+# def basic_op(operator, value1, value2):
+#     if operator == '+':
+#         return value1 + value2
+#     elif operator == '-':
+#         return value1 - value2
+#     elif operator =='*':
+#         return value1 * value2
+#     elif operator == '/':
+#         return value1 / value2
+
+# codewars 5
+# def sum_array(a):
+#     return sum(a)
+# sum_array([2,3,6,1,-2])
+
+# codewars 6 
+# def simple_multiplication(number) :
+#     if number % 2 == 0:
+#         return number * 8
+#     else:
+#         return number *  9
