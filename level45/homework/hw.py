01bc9a2a5551a60500987bb82e4407ddcde37740
zazamchedldize <mchedlidze.zaza1000@gmail.com>
2026-03-06 16:06:13 +0400

    second commit

---
+++ level45/homework/hw.py
@@ -0,0 +1,28 @@
+# codewars 1
+# def accum(st):
+#     word = ""
+    
+#     for i in range(len(st)):
+#         word += st[i].upper() + st[i].lower() * i
+        
+#         if i != len(st) - 1:
+#             word += "-"
+    
+#     return word
+
+# codewars 2
+# def litres(time):
+#     return int(time * 0.5)
+
+# codewars 3
+
+# codewars 4 
+# def lovefunc(flower1, flower2):
+#     return (flower1 % 2) != (flower2 % 2)
+
+# codewars 5
+# def maps(arr):
+#     result = []
+#     for i in arr:
+#         result.append(i * 2)
+#     return result
