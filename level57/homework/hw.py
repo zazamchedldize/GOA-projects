4148510f75ce06865c290795a4d2c3b3fd4f1681
zazamchedldize <mchedlidze.zaza1000@gmail.com>
2026-04-19 15:50:30 +0400

    second commit

---
+++ level57/homework/hw.py
@@ -0,0 +1,18 @@
+# codewars 1
+# https://www.codewars.com/kata/525f50e3b73515a6db000b83
+# def create_phone_number(n):
+#     return "(" + str(n[0]) + str(n[1]) + str(n[2]) + ") " + str(n[3]) + str(n[4]) + str(n[5]) + "-" + str(n[6]) + str(n[7]) + str(n[8]) + str(n[9])
+
+# codewars 3
+#  https://www.codewars.com/kata/5aa1bcda373c2eb596000112
+# def max_tri_sum(numbers):
+#     return sum(sorted(set(numbers), reverse=True)[:3])
+
+# codewars 4
+# https://www.codewars.com/kata/5783d8f3202c0e486c001d23
+# def to_float_array(arr):
+#     result = []
+#     for i in arr:
+#         result.append(float(i))
+#     return result
+
