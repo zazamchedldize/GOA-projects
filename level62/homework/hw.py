b6149389fbe02f8162ee5ed03da27afaa911281a
zazamchedldize <mchedlidze.zaza1000@gmail.com>
2026-05-03 18:16:28 +0400

    second commit

---
+++ level62/homework/hw.py
@@ -0,0 +1,7 @@
+# codewars 1
+
+
+# codewars 2
+# https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/python
+# def create_phone_number(n):
+#     return "(" + str(n[0]) + str(n[1]) + str(n[2]) + ") " + str(n[3]) + str(n[4]) + str(n[5]) + "-" + str(n[6]) + str(n[7]) + str(n[8]) + str(n[9])
